129a41fb1feb2de6cce072c56e4a1765b8a08952
Ali <alichavoshii1372@gmail.com>
2021-02-07 08:48:50 +0330

    Add project files.

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
