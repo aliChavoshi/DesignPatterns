dceda2ce4edb91ac0abff1112cb333a6322097b2
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-11-27 14:05:26 +0330

    add singleton pattern with lock

---
+++ DesignPatterns/Singleton/ConfigurationManager.cs
@@ -0,0 +1,33 @@
+﻿namespace DesignPatterns.Singleton;
+
+public sealed class ConfigurationManager
+{
+    private static ConfigurationManager? Instance { get; set; }
+    private static readonly Lock Lock = new();
+    public static Dictionary<string, string>? Settings { get; private set; }
+
+    private ConfigurationManager()
+    {
+        //Add Configuration in the private constructor
+        Settings = new Dictionary<string, string>
+        {
+            { "AppName", "MyApp" },
+            { "Version", "1.0" },
+            { "Database", "SQL Server" }
+        };
+    }
+
+    public static ConfigurationManager GetInstance
+    {
+        get
+        {
+            if (Instance != null) return Instance;
+            lock (Lock)
+            {
+                Instance ??= new ConfigurationManager();
+            }
+
+            return Instance;
+        }
+    }
+}
