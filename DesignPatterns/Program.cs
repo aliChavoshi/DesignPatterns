b9bc501fb24c6453f0fad6ba7452c40c7d44654f
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-12-01 14:46:15 +0330

    Add Builder Pattern implementation for Computer creation
    Introduced a `Computer` class with properties for `CPU`, `RAM`,
    `Storage`, and `GPU`, along with a `Display` method to show
    specifications.
    
    Added `GamingComputerBuilder` and `OfficeComputerBuilder`
    classes implementing the `IComputerBuilder` interface to
    construct `Computer` objects step-by-step.
    
    Created the `IComputerBuilder` interface to define the contract
    for building `Computer` objects.
    
    Updated `Program.cs` to demonstrate the Builder Pattern with
    an example of creating and displaying a `GamingComputer`.
    Commented out the previous Factory Method Pattern example.

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,5 +1,16 @@
-﻿using DesignPatterns.Factory_Method._1;
+﻿using DesignPatterns.Builder._1;
+using DesignPatterns.Factory_Method._1;
 
-new RoadLogistics().PlanDelivery("A");
-new SeaLogistics().PlanDelivery("B");
-new PlaneLogistics().PlanDelivery("C");
+//Factory Method Pattern
+// new RoadLogistics().PlanDelivery("A");
+// new SeaLogistics().PlanDelivery("B");
+// new PlaneLogistics().PlanDelivery("C");
+
+//Builder Pattern
+var gamingComputer = new GamingComputerBuilder()
+    .SetCpu("Core i9")
+    .SetGpu("NVIDIA GeForce RTX 3080")
+    .SetRam("32GB")
+    .SetStorage("1TB SSD")
+    .Build();
+gamingComputer.Display();
