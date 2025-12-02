06e2b76497e9869eef3a5405931b46cc34edb94d
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-12-02 16:27:02 +0330

    add new exmaple of the builder pattern

---
+++ DesignPatterns/Builder/2/EmailBuilder.cs
@@ -0,0 +1,35 @@
+﻿namespace DesignPatterns.Builder._2;
+
+public class EmailBuilder : IEmailBuilder
+{
+    private readonly Email _email = new();
+
+    public IEmailBuilder SetTo(string to)
+    {
+        _email.To = to;
+        return this;
+    }
+
+    public IEmailBuilder SetSubject(string subject)
+    {
+        _email.Subject = subject;
+        return this;
+    }
+
+    public IEmailBuilder SetBody(string body)
+    {
+        _email.Body = body;
+        return this;
+    }
+
+    public IEmailBuilder SetAttachments(string attachment)
+    {
+        _email.Attachments.Add(attachment);
+        return this;
+    }
+
+    public Email Build()
+    {
+        return _email;
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,5 +1,5 @@
 ﻿using DesignPatterns.Builder._1;
-using DesignPatterns.Factory_Method._1;
+using DesignPatterns.Builder._2;
 
 //Factory Method Pattern
 // new RoadLogistics().PlanDelivery("A");
@@ -7,10 +7,27 @@ using DesignPatterns.Factory_Method._1;
 // new PlaneLogistics().PlanDelivery("C");
 
 //Builder Pattern
-var gamingComputer = new GamingComputerBuilder()
-    .SetCpu("Core i9")
-    .SetGpu("NVIDIA GeForce RTX 3080")
-    .SetRam("32GB")
-    .SetStorage("1TB SSD")
+// var gamingComputer = new GamingComputerBuilder()
+//     .SetCpu("Core i9")
+//     .SetGpu("NVIDIA GeForce RTX 3080")
+//     .SetRam("32GB")
+//     .SetStorage("1TB SSD")
+//     .Build();
+// gamingComputer.Display();
+//
+// var officeComputer = new OfficeComputerBuilder()
+//     .SetCpu("Core i5")
+//     .SetGpu("Integrated Graphics")
+//     .SetRam("16GB")
+//     .SetStorage("512GB SSD")
+//     .Build();
+// officeComputer.Display();
+
+//Builder Pattern - Email Example
+var email = new EmailBuilder()
+    .SetTo("recipient@example.com")
+    .SetSubject("Hello")
+    .SetAttachments("file1.txt")
+    .SetAttachments("file2.jpg")
     .Build();
-gamingComputer.Display();
+email.Send();
